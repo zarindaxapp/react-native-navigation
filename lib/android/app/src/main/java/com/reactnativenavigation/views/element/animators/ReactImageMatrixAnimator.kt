a71b91764d8b02c2b33117f44615a66125da77c3
Ward Abbass <warda@wix.com>
2021-08-02 15:46:37 +0300

    refactor return type to no nullable (#7211)
    Co-authored-by: Yogev Ben David <yogev132@gmail.com>

--- lib/android/app/src/main/java/com/reactnativenavigation/views/element/animators/ReactImageMatrixAnimator.kt
+++ lib/android/app/src/main/java/com/reactnativenavigation/views/element/animators/ReactImageMatrixAnimator.kt
@@ -8,6 +8,7 @@ import android.graphics.Rect
 import android.view.View
 import com.facebook.drawee.drawable.ScalingUtils
 import com.facebook.drawee.drawable.ScalingUtils.InterpolatingScaleType
+import com.facebook.react.views.image.ImageResizeMode
 import com.facebook.react.views.image.ReactImageView
 import com.reactnativenavigation.options.SharedElementTransitionOptions
 import com.reactnativenavigation.utils.ViewUtils
@@ -50,12 +51,14 @@ class ReactImageMatrixAnimator(from: View, to: View) : PropertyAnimatorCreator<R
         }
     }
 
-    private fun getScaleType(child: View): ScalingUtils.ScaleType? {
-        return getScaleType(child as ReactImageView, child.hierarchy.actualImageScaleType!!)
+    private fun getScaleType(child: View): ScalingUtils.ScaleType {
+        return getScaleType(
+            child as ReactImageView, child.hierarchy.actualImageScaleType ?: ImageResizeMode.defaultValue()
+        )
     }
 
-    private fun getScaleType(child: ReactImageView, scaleType: ScalingUtils.ScaleType): ScalingUtils.ScaleType? {
-        if (scaleType is InterpolatingScaleType) return getScaleType(child, scaleType.scaleTypeTo )
+    private fun getScaleType(child: ReactImageView, scaleType: ScalingUtils.ScaleType): ScalingUtils.ScaleType {
+        if (scaleType is InterpolatingScaleType) return getScaleType(child, scaleType.scaleTypeTo)
         return scaleType
     }
 
