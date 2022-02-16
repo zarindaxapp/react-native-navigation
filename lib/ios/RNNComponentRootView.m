1394d5017e6b9a02f493312ea0fc61aff2309c19
Danilo Bürger <info@danilobuerger.de>
2022-02-16 11:51:25 +0100

    Revert "Remove unneeded RCTUIManager setAvailableSize call (#7412)" (#7458)
    This reverts commit 345344a2c27611c6b3dc24f7a54742b2df25a7cc.

--- lib/ios/RNNComponentRootView.m
+++ lib/ios/RNNComponentRootView.m
@@ -2,6 +2,20 @@
 
 @implementation RNNComponentRootView
 
+- (instancetype)initWithBridge:(RCTBridge *)bridge
+                    moduleName:(NSString *)moduleName
+             initialProperties:(NSDictionary *)initialProperties
+                  eventEmitter:(RNNEventEmitter *)eventEmitter
+           reactViewReadyBlock:(RNNReactViewReadyCompletionBlock)reactViewReadyBlock {
+    self = [super initWithBridge:bridge
+                      moduleName:moduleName
+               initialProperties:initialProperties
+                    eventEmitter:eventEmitter
+             reactViewReadyBlock:reactViewReadyBlock];
+    [bridge.uiManager setAvailableSize:UIScreen.mainScreen.bounds.size forRootView:self];
+    return self;
+}
+
 - (NSString *)componentType {
     return ComponentTypeScreen;
 }
