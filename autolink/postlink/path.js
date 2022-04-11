ff03c2ab610a366dba74338919627d598d6238ca
Yogev Ben David <yogev132@gmail.com>
2022-04-11 15:54:36 +0300

    Fix auto link on react-native 68+ (#7500)

--- autolink/postlink/path.js
+++ autolink/postlink/path.js
@@ -1,5 +1,7 @@
 var glob = require('glob');
-var ignoreFolders = {ignore: ['node_modules/**', '**/build/**', '**/Build/**', '**/DerivedData/**', '**/*-tvOS*/**']};
+var ignoreFolders = {
+  ignore: ['node_modules/**', '**/build/**', '**/Build/**', '**/DerivedData/**', '**/*-tvOS*/**'],
+};
 
 var manifestPath = glob.sync('**/AndroidManifest.xml', ignoreFolders)[0];
 
@@ -9,6 +11,10 @@ var mainApplicationJava = glob.sync('**/MainApplication.java', ignoreFolders)[0]
 exports.mainApplicationJava = mainApplicationJava;
 exports.rootGradle = mainApplicationJava.replace(/android\/app\/.*\.java/, 'android/build.gradle');
 
-exports.appDelegate = glob.sync('**/AppDelegate.m', ignoreFolders)[0];
+var reactNativeVersion = require('../../../react-native/package.json').version;
+exports.appDelegate = glob.sync(
+  reactNativeVersion < '0.68.0' ? '**/AppDelegate.m' : '**/AppDelegate.mm',
+  ignoreFolders
+)[0];
 exports.podFile = glob.sync('**/Podfile', ignoreFolders)[0];
 exports.plist = glob.sync('**/info.plist', ignoreFolders)[0];
