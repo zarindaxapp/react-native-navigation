ea07617fe7d2b49ab5c3a12987e3339b46822eb2
zarindaxapp <115018530+zarindaxapp@users.noreply.github.com>
2023-03-15 10:45:04 +0330

    Fix interceptTouchOutside issue

--- lib/android/app/src/main/java/com/reactnativenavigation/utils/MotionEvent.kt
+++ lib/android/app/src/main/java/com/reactnativenavigation/utils/MotionEvent.kt
@@ -3,11 +3,19 @@ package com.reactnativenavigation.utils
 import android.graphics.Rect
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewGroup
 
 val hitRect = Rect()
 
 fun MotionEvent.coordinatesInsideView(view: View?): Boolean {
     view ?: return false
-    view.getHitRect(hitRect)
-    return hitRect.contains(x.toInt(), y.toInt())
+    
+    val viewGroup = (view as ViewGroup).getChildAt(0) as ViewGroup
+    return if (viewGroup.childCount > 0) {
+        val content = viewGroup.getChildAt(0)
+        content.getHitRect(hitRect)
+        hitRect.contains(x.toInt(), y.toInt())
+    } else {
+        false
+    }
 }
